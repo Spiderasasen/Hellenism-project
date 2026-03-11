2ed6e0812f6c321481968db668a40b0f8b65e2c1
spiderasasen <spiderasasen@gmail.com>
2026-03-11 16:13:33 -0400

    made a basic skeliton for the project

--- Hellenism/src/Page/Home.jsx
+++ Hellenism/src/Page/Home.jsx
@@ -1,7 +1,36 @@
 function Home(){
     return (
         <div>
-            <h1>Hello World</h1>
+            {/*main title*/}
+            <h1>Hellenism</h1>
+
+            {/*button layer will go here*/}
+            <div>
+                <button>Myth</button>
+                <button>Gods</button>
+                <button>Retrials</button>
+                <button>Holiday</button>
+                <button>Prayers</button>
+            </div>
+
+            {/*mini welcome message*/}
+            <div>
+                <h3>Welcome to Hellenism</h3>
+                <p>
+                    Here you will learn that yes, we still worship the Greek gods.
+                    This might sound like a taboo, which if that's the case, im sorry you think that.
+                    But if you are curios to learn more of our religion, then we will welcome you with open arms.
+                    This website will tell you how i see this religion, so if you still skeptical, dont be for as ill write with the upmost respect to the gods as a mere mortal can.
+                </p>
+            </div>
+
+            {/*talking about the religion*/}
+            <div>
+                <h3>About the religion</h3>
+                <p>
+                    Even though its an old religion, it still very hidden on what we can and cant do.
+                </p>
+            </div>
         </div>
     )
 }
